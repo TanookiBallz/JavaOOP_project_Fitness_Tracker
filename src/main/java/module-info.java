d43344e10741abfbfaf0d1d394a9c6e33ad74ea4
TanookiBallz <166801942+TanookiBallz@users.noreply.github.com>
2024-12-02 15:57:01 +0600

    dataBase connected DataManager class created

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module org.example.fitness_tracker {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
